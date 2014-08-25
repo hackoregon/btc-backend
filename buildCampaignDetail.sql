a0fc15d8d822d5e8e842f88c36731d3da0e559c2
samhiggins2001 <higgins2001@hotmail.com>
2014-08-24 21:03:53 -0700

    added campaign_detail dump and import

---
+++ buildCampaignDetail.sql
@@ -0,0 +1,14 @@
+﻿
+create table campaign_detail
+(candidate_name text, 
+race text, 
+website text, 
+phone text, 
+total double precision, 
+grassroots double precision,
+instate double precision, 
+committee_names text, 
+filer_id integer);
+
+copy campaign_detail from '/home/ubuntu/gitforks/backend/campaign_detail_dump.csv'
+WITH CSV HEADER QUOTE as '"';
