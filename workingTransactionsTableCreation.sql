dda05a81def57d521adf5dfa629a10d8c3b8b48a
samhiggins2001 <higgins2001@hotmail.com>
2014-09-10 11:06:18 -0700

    added workingTransactionsTableCreation.sql

---
+++ workingTransactionsTableCreation.sql
@@ -0,0 +1,19 @@
+﻿
+drop table if exists direction_codes;
+create table direction_codes (
+sub_type varchar,
+direction varchar(7)
+);
+
+copy direction_codes from '/Users/samhiggins2001_worldperks/prog/hack_oregon/moneyDirectionCodes.txt' with (format csv, delimiter E'\t');
+
+drop table if exists working_trasactions;
+create table working_trasactions
+	as (
+		select tran_id, tran_date, filer, contributor_payee, rct.sub_type, amount, 
+		contributor_payee_committee_id, filer_id, purp_desc, book_type, addr_line1, 
+		addr_line2, city, state, zip, purpose_codes, dc.direction as direction
+		from raw_committee_transactions rct
+		join direction_codes dc
+		on dc.sub_type = rct.sub_type
+		);
