dd37181461fd8c810df8f56a6c9271f59f3de993
samhiggins2001 <higgins2001@hotmail.com>
2014-10-09 04:12:50 -0700

    fixed slider endpoint

--- endpoints/sliderEndPoint/sliderEndPointTableCreation.sql
+++ endpoints/sliderEndPoint/sliderEndPointTableCreation.sql
@@ -1,22 +1,33 @@
 ﻿drop table if exists candidate_sum_by_date;
 create table candidate_sum_by_date as
-(select min.filer_id as filer_id, min.tran_date as tran_date, min.total as money_in, mout.total as money_out
-from 
-	(select filer_id, tran_date, sum(amount) as total 
-	from cc_working_transactions 
-	where direction = 'in'
-	group by tran_date, filer_id) as min
-join  
-	(select filer_id, tran_date, sum(amount) as total 
-	from cc_working_transactions 
-	where direction = 'out'
-	group by tran_date, filer_id) as mout
-on min.tran_date = mout.tran_date
-and min.filer_id = mout.filer_id);
+(select all_in.filer_id as filer_id, all_in.tran_date as tran_date, total_in, total_out
+from
+	(select sub.filer_id as filer_id, sub.tran_date as tran_date, total_in
+	from	(select filer_id, tran_date, sum(amount) as total_in 
+		from cc_working_transactions 
+		where direction = 'in'
+		group by tran_date, filer_id) as sub0
+	right join
+		(select distinct filer_id, tran_date 
+		from cc_working_transactions) as sub
+	on sub0.filer_id = sub.filer_id
+	and sub0.tran_date = sub.tran_date) as all_in
+join
+	(select sub.filer_id as filer_id, sub.tran_date as tran_date, total_out
+	from	(select filer_id, tran_date, sum(amount) as total_out 
+		from cc_working_transactions 
+		where direction = 'out'
+		group by tran_date, filer_id) as sub0
+	right join
+		(select distinct filer_id, tran_date 
+		from cc_working_transactions) as sub
+	on sub0.filer_id = sub.filer_id
+	and sub0.tran_date = sub.tran_date) as all_out
+on all_in.filer_id = all_out.filer_id
+and all_in.tran_date = all_out.tran_date);
+
 
 /*select * from candidate_by_date;*/
-
-
 DROP FUNCTION IF EXISTS http.get_candidate_sum_by_date(name1 text, name2 text, commID text, name4 text);
 CREATE FUNCTION http.get_candidate_sum_by_date(name1 text, name2 text, commID text, name4 text) RETURNS json AS $$
 DECLARE
