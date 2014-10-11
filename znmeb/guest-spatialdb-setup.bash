194a4ce3ae7ff19dc6be6b2da2a037343a002848
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-10 21:08:06 -0700

    don't put in the extensions - restore does that

--- znmeb/guest-spatialdb-setup.bash
+++ znmeb/guest-spatialdb-setup.bash
@@ -9,12 +9,6 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
-# install the core extensions
-sudo su - postgres -c \
-  "psql -c 'CREATE EXTENSION IF NOT EXISTS adminpack;'"
-sudo su - postgres -c \
-  "psql -c 'CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;'"
-
 # create a non-root user - can can log in and create schemas/tables only!
 sudo su - postgres -c "dropdb ${USER}"
 sudo su - postgres -c "dropdb or_geocoder"
@@ -37,25 +31,6 @@ do
   done
 done
 
-# add the extensions to the geocoder databases
-for j in or us
-do
-  for i in \
-    "CREATE EXTENSION IF NOT EXISTS postgis;" \
-    "CREATE EXTENSION IF NOT EXISTS postgis_topology;" \
-    "CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;" \
-    "CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;" \
-    "GRANT USAGE ON SCHEMA tiger TO PUBLIC;" \
-    "GRANT USAGE ON SCHEMA tiger_data TO PUBLIC;" \
-    "GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA tiger TO PUBLIC;" \
-    "GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA tiger_data TO PUBLIC;" \
-    "GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA tiger TO PUBLIC;" \
-    "ALTER DEFAULT PRIVILEGES IN SCHEMA tiger_data GRANT SELECT, REFERENCES ON TABLES TO PUBLIC;"
-  do
-    sudo su - postgres -c "psql -d ${j}_geocoder -c '${i}'"
-  done
-done
-
 # create the 'districts' database in the 'spatial' tablespace
 for i in \
   "DROP DATABASE IF EXISTS districts;" \
@@ -64,10 +39,8 @@ do
   sudo su - postgres -c "psql -d postgres -c '${i}'"
 done
 
-# just add basic geo extensions to 'districts'
+# create the 'districts' schema
 for i in \
-  "CREATE EXTENSION IF NOT EXISTS postgis;" \
-  "CREATE EXTENSION IF NOT EXISTS postgis_topology;" \
   "CREATE SCHEMA districts AUTHORIZATION ${USER};"
 do
   sudo su - postgres -c "psql -d districts -c '${i}'"
