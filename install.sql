d59a7c9cffa18789e989e66ffbc690d7eeb06fb0
samhiggins2001 <higgins2001@hotmail.com>
2014-10-30 02:12:48 -0700

    get db stats endpoints added

--- install.sql
+++ install.sql
@@ -6,6 +6,24 @@ CREATE FUNCTION http.get_args2(name1 text, name2 text, name3 text, name4 text, O
   'SELECT row_to_json(row(name1 , name2 ,  name3 , name4));'
 LANGUAGE SQL;
 
+CREATE FUNCTION http.get_db_stat(name1 text, name2 text, tabname text, name4 text) RETURNS json AS $$
+DECLARE
+  result json;
+BEGIN
+
+  SELECT array_to_json(array_agg(row_to_json(qres, true)), true)
+  FROM 
+    (SELECT * 
+    FROM hack_oregon_db_status
+    ORDER BY date ASC
+    LIMIT 3) qres
+  INTO result;
+  
+  return result;
+END;
+$$ LANGUAGE plpgsql;
+
+
 CREATE FUNCTION http.get_all_documentation(name1 text, name2 text, commID text, name4 text) RETURNS json AS $$
 DECLARE
   result json;
